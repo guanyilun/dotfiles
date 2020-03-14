b474ea860612a8703ed541f8af6029e7c67856fd
Yilun Guan <zoom.aaron@gmail.com>
2020-03-13 22:14:23 -0400

    polybar: update config

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -7,4 +7,11 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar default &
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload top &
+    done
+else
+    polybar --reload top &
+fi
